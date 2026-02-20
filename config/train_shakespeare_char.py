1be7f22f480fd4c37dc26403503126d73b5abbc1
Colab_elias <eliasls2002@yahoo.com>
2026-02-20 00:57:18 +0000

    training baseline and learning collab tunnel plus package mangement

--- config/train_shakespeare_char.py
+++ config/train_shakespeare_char.py
@@ -7,13 +7,13 @@ eval_iters = 200
 log_interval = 10 # don't print too too often
 
 # we expect to overfit on this small dataset, so only save when val improves
-always_save_checkpoint = False
+always_save_checkpoint = True
 
-wandb_log = False # override via command line if you like
+wandb_log = True # override via command line if you like
 wandb_project = 'shakespeare-char'
 wandb_run_name = 'mini-gpt'
 
-dataset = 'shakespeare_char'
+dataset = 'shakespeare'
 gradient_accumulation_steps = 1
 batch_size = 64
 block_size = 256 # context of up to 256 previous characters
@@ -33,5 +33,5 @@ beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
 warmup_iters = 100 # not super necessary potentially
 
 # on macbook also add
-# device = 'cpu'  # run on cpu only
-# compile = False # do not torch compile the model
+device = 'cuda'  # run on cpu only
+compile = True # do not torch compile the model
